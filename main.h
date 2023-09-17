d4e614a46142938a6f90f38502e5c19fda10d348
JayJust0h <edwinjustus11@gmail.com>
2023-09-17 03:55:40 -0700

    printy.c

--- main.h
+++ main.h
@@ -2,28 +2,8 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
-/**
- * struct flags - Contains flags to consider
- * when flag specifier is passed.
- * @plus: Flag rep '+' character
- * @space: Flag rep ' ' character
- * @hash: Flag rep '#' character.
- */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-/**
- * struct printHandler - Chooses the right function.
- * @c: Format specifier.
- * @f: Pointer for the correct printing function.
- */
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int putcher(char c);
