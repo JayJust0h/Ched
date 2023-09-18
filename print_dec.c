0ad36c108c8c2bc79b8e84c6f7ccaa4065e057e9
nyambuukim <nyambuukim1@gmail.com>
2023-09-18 15:02:20 -0700

    decimals

--- print_dec.c
+++ print_dec.c
@@ -11,5 +11,39 @@ int print_dec(va_list d)
 	int length, baseten, number, a, count = 0, h, digit;
 
 	h = va_arg(d, int);
-
+	if (h != 0)
+	{
+		if (h < 0)
+		{
+			_putchear('-');
+			count++;
+		}
+		number = h;
+		length = 0;
+		while (number != 0)
+		{
+			number /= 10;
+			length++;
+		}
+		baseten = 1;
+		for (a = 1; a <= length - 1; a++)
+			baseten *= 10;
+		for (a = 1 a <= length; a++)
+		{
+			digit = h / baseten;
+			if (n < 0)
+				_putchear((digit * -1) + 48);
+			else
+				_putchear(digit + '0');
+			count++;
+			n -= digit * baseten;
+			baseten /= 10;
+		}
+	}
+	else
+	{
+		_putchear('0');
+		return (1);
+	}
+	return (count);
 }
