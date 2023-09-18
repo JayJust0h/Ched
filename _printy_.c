2e5a7a88945a9b65386a2be2069dfcc3a1f5b9f2
JayJust0h <edwinjustus11@gmail.com>
2023-09-18 05:30:40 -0700

    wow

--- _printy_.c
+++ _printy_.c
@@ -1,4 +1,5 @@
 	#include <stdarg.h>
+#include "main.h"
 	#include <stddef.h>
 	#include <string.h>
 	#include <unistd.h>
@@ -7,52 +8,56 @@ _printf - Printf function
  * @format: format string
  * Return: Number of characters printed
 */
-	int _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	int chac_prnt = 0;
-	
-	va_list nu_of_args;
-	if (format == NULL)
-	return(-1);
-	
-	va_start(nu_of_args, format);
-	while(*format)
-	{
-	if (*format != '%')
-	{
-	write(1, format, 1);
-	chac_prnt++;
-	}
-	else
-	{
-	format++;
-	if (*format == '\0')
-	break;
-	if (*format == '%')
-	{
-	write(1, format, 1);
-	chac_prnt++;
-	}
-	else if (*format == 'c')
-	{
-	char c = (va_arg(nu_of_args, int));
-	write(1, &c, 1);
-	chac_prnt++;
-	}
-	else if (*format == 's')
-	{
-	char *strn = va_arg(nu_of_args, char*);
-	int str_len = 0;
-	
-	while (strn[str_len] != '\0')
-	str_len++;
-	write(1, strn, str_len);
-	chac_prnt = str_len;
-	}
-	format++;
-	}
-	va_end(nu_of_args);
-	}
-	return (chac_prnt);
-	
+    int chac_prnt = 0;
+    va_list nu_of_args;
+
+    if (format == NULL)
+        return -1;
+
+    va_start(nu_of_args, format);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            chac_prnt++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%')
+            {
+                write(1, format, 1);
+                chac_prnt++;
+            }
+            else if (*format == 'c')
+            {
+                char c = (va_arg(nu_of_args, int));
+                write(1, &c, 1);
+                chac_prnt++;
+            }
+            else if (*format == 's')
+            {
+                char *strn = va_arg(nu_of_args, char*);
+                int str_len = 0;
+
+                while (strn[str_len] != '\0')
+                    str_len++;
+                write(1, strn, str_len);
+                chac_prnt = str_len;
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                chac_prnt++;
+            }
+            format++;
+        }
+    }
+    va_end(nu_of_args);
+    return chac_prnt;
 }
