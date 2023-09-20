9e4fcb2d799ed0af6e883478944bdb15d917f906
nyambuukim <nyambuukim1@gmail.com>
2023-09-19 22:59:21 -0700

    rot13

--- print_rot13.c
+++ print_rot13.c
@@ -1,34 +1,37 @@
 #include "main.h"
 
 /**
-  * print_rot13 - prints string using the rot13.
-  * @R: To convert string
-  * Return: length output
-  * Authors: Tinnie & Jay
-  */
+ * print_rot13 - prints a string using the ROT13 cipher.
+ * @R: A variable argument list containing the string to convert.
+ * Return: The number of characters printed.
+ * Authors: Tinnie & Jay
+ */
 int print_rot13(va_list R)
 {
 	int a, h, count = 0;
 	char *r;
 	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
+	
 	r = va_arg(R, char *);
-	/* If the input string is NULL, replace it with "(null)" */
 	if (r == NULL)
 		r = "(null)";
 	for (a = 0; r[a] != '\0'; a++)
 	{
-		for (h = 0; input[h] != '\0'; h++)
-		{
-			if (r[a] == input[h])
-			{
-				/*Print The Rot 13 char */
-				putchear(output[h]);
-				count++;
-				break;
-			}
-		}
+		char c = r[a];
+		char converted = c;
+	if (c >= 'A' && c <= 'Z')
+	{
+		converted = output[c - 'A'];
 	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		converted = output[c - 'a' + 26];
+	}
+	
+	putchear(converted);
+	count++;
+	}
+	
 	return (count);
 }
